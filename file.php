1fc80b75a5045eceb52d3b78964181e416a2a25e
oktiaraaf <87087131+oktiaraaf@users.noreply.github.com>
2021-07-07 23:50:21 +0700

    Add files via upload

---
+++ file.php
@@ -0,0 +1,39 @@
+<section class="gpd-section">
+  <div class="gpd-container gpd-cnt" id="iq0tcf">
+    <h1 id="ikb9j" data-anim-type="fadeInUp" class="gpd-header">KALKULATOR
+    </h1>
+  </div>
+</section>
+<style>html {
+  scroll-behavior: smooth;
+  }
+</style>
+<div class="gpd-text" id="iv0rh">Input bilangan
+</div>
+<section class="gpd-section" id="iflbp">
+  <div class="gpd-container gpd-cnt">
+    <form method="post" id="ifv4g" data-redirect="" class="form">
+      <div class="form-group">
+        <label class="label">Masukan bilangan pertama</label>
+        <input type="number" placeholder="Masukan bilangan pertama" name="bilangan pertama" class="input"/>
+      </div>
+      <div class="form-group">
+        <label class="label">Masukan bilangan kedua</label>
+        <input type="number" placeholder="Masukan bilangan kedua" name="bilangan kedua" class="input"/>
+      </div>
+      <div class="form-group">
+        <label class="label"></label>
+        <select type="text" name="" class="select"><option value="1">+</option><option value="2">-</option><option value="3">/</option><option value="4">*</option></select>
+      </div>
+      <div class="form-group">
+        <button type="submit" class="button" id="i4xik">Hasil</button>
+      </div>
+      <div data-form-state="success" class="state-success" id="ixmnk">Thanks! We received your request
+      </div>
+      <div data-form-state="error" class="state-error" id="i63yx">An error occurred on processing your request, try again!
+      </div>
+    </form>
+  </div>
+</section>
+<div class="gpd-text" id="ilrjj">Nilai bilangan:&nbsp; &nbsp;&nbsp;
+</div>
